4dd9fbf9c825798062e592e585c3105732f70b82
ralph.gutmann <ralph.gutmann@mercedes-benz.com>
2023-04-04 17:10:46 +0200

    window title

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,14 @@ use bevy::{prelude::*};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "Cube-Mania!    ---> use cursor keys to move, Esc to quit <---".to_string(),
+            ..default()
+        }),
+        ..default()
+    }))
+    .add_startup_system(setup)
         .add_system(movement)
         .add_system(bevy::window::close_on_esc)
         .run();
