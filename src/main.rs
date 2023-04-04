cb950a63f6fe4089d1cfe3ef277e12a3193d5842
ralph.gutmann <ralph.gutmann@mercedes-benz.com>
2023-04-04 18:48:33 +0200

    asteroid belt impl

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::f32::consts::TAU;
 use bevy::{prelude::*};
+use rand::prelude::*;
 
 fn main() {
     App::new()
@@ -26,18 +27,24 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     // plane
+    let plane_size = 5.0;
     commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
+        mesh: meshes.add(shape::Plane::from_size(plane_size).into()),
         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..default()
     });
     // cubes in a row
-    let cube_count = 10;
+    let cube_count = 50;
+    let mut rng = rand::thread_rng();
     for i in 1..=cube_count {
+        let mut position = Transform::from_xyz(rng.gen_range((plane_size*0.2)..(plane_size*0.4)),rng.gen_range(0.3..0.7),0.0);
+        position.translate_around(Vec3::ZERO, Quat::from_axis_angle(Vec3::Y, -TAU / cube_count as f32 * i as f32));
+
         commands.spawn((PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Cube { size: 0.12 })),
-            material: materials.add(calc_rainbow_color(0, 10, i-1).into()),
-            transform: Transform::from_xyz((i as f32 - 5.0) / 2.0 - 0.25, 0.5, 0.0),
+            material: materials.add(calc_rainbow_color(0, cube_count, i-1).into()),
+            //transform: Transform::from_xyz((i as f32 - 5.0) / 2.0 - 0.25, 0.5, 0.0),
+            transform: position,
             ..default()
         },
         Movable,));
@@ -58,6 +65,7 @@ fn setup(
         ..default()
     });
 }
+
 
 fn movement(
     input: Res<Input<KeyCode>>,
@@ -94,12 +102,12 @@ fn movement(
         transform.rotate_x(rotation_vec.x * TAU * time.delta_seconds() * 50.0);
         transform.rotate_y(rotation_vec.y * TAU * time.delta_seconds() * 50.0);
         transform.rotate_z(rotation_vec.z * TAU * time.delta_seconds() * 50.0);
-
     }
 }
 
+
 fn calc_rainbow_color(min :usize, max :usize, val :usize) -> Color {
-    let min_hue = 240.0;
+    let min_hue = 360.0;
     let max_hue = 0.0;
     let cur_percent = (val - min) as f32 / (max - min) as f32;
     Color::hsl(((cur_percent * (max_hue-min_hue) ) + min_hue) as f32, 1.0, 0.5)
